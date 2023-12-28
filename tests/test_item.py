fcf6cd53e5a6a552be02148152e1ffac07469bf1
ncgordeev <ncgordeev@gmail.com>
2023-12-28 21:47:03 +0300

    add test for apply_discount

--- tests/test_item.py
+++ tests/test_item.py
@@ -15,3 +15,17 @@ from src.item import Item
 def test_calculate_total_price(instance, expected):
     total_price = instance.calculate_total_price()
     assert total_price == expected
+
+
+@pytest.mark.parametrize(
+    "instance, pay_ratio, expected",
+    [
+        (Item("Смартфон", 20, 10), 0.5, 10.0),
+        (Item("Ноутбук", 200, 50), 0.1, 20.0),
+        (Item("Пылесос", 100, 0), 0.9, 90),
+    ]
+)
+def test_apply_discount(instance, pay_ratio, expected):
+    instance.pay_rate = pay_ratio
+    instance.apply_discount()
+    assert instance.price == expected
