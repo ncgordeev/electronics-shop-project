632f9c390f9f423a4a218977abd33163f6a953ec
ncgordeev <ncgordeev@gmail.com>
2023-12-28 21:19:08 +0300

    add test for calculate_total_price

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,17 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+import pytest
+
+from src.item import Item
+
+
+@pytest.mark.parametrize(
+    "instance, expected",
+    [
+        (Item("Смартфон", 20, 10), 200),
+        (Item("Ноутбук", 200, 50), 10000),
+        (Item("Пылесос", 10, 0), 0),
+    ]
+)
+def test_calculate_total_price(instance, expected):
+    total_price = instance.calculate_total_price()
+    assert total_price == expected
